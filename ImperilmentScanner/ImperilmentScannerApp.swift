255724009a928a7e3c7bc22a0211d85052d646ab
Todd Southen <tsouthen@gmail.com>
2023-01-31 19:55:09 -0800

    Initial Commit

---
+++ ImperilmentScanner/ImperilmentScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImperilmentScannerApp.swift
+//  ImperilmentScanner
+//
+//  Created by Todd Southen on 2023-01-31.
+//
+
+import SwiftUI
+
+@main
+struct ImperilmentScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
